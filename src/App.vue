d072a8b0a89beb1446fe2f3e66fbd912605d8b54
thomasguillet44 <guilletthomas59@gmail.com>
2026-02-10 17:22:11 +0100

    test deploy

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
 <template>
   <h1>You did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Bonjour serveur
   </p>
 </template>
 
