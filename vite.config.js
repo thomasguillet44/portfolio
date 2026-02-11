1dc5128ce5e722de92b36d51ef4c843d7438a16d
thomasguillet44 <guilletthomas59@gmail.com>
2026-02-11 18:34:39 +0100

    retrait import svg loader dans vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
 export default defineConfig({
